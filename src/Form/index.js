68ec2c011e998fd045fabef38eea71fda9b35e0c
Piyush Bajaj <45912468+piyush-bajaj@users.noreply.github.com>
2020-05-02 23:06:15 +0530

    Form component and state introduction

---
+++ src/Form/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+	render() {
+		return (
+			<form action="">
+				<input type="text" placeholder="Github username"></input>
+				<button>Add card</button>
+			</form>
+		);
+	}
+}
+
+export default Form;
